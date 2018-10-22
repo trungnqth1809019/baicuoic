04c3f2624ec6ebaea92b153fca64e278e1eadd59
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-22 22:24:05 +0700

    bai tap 2

--- main.c
+++ main.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
 #include <mem.h>
-float tinh(int ia, int idai){
-    return     (float) ia * 100 / idai;
+
+char chuoi[5] = {'a', 'e', 'i', 'o', 'u'};
+char string[50];
+int idai, idem[5] = {0, 0, 0, 0, 0};
+float iphantram[5];
+
+float tinh(int ia, int idai) {
+    return (float) ia * 100 / idai;
+}
+
+void dem(int k) {
+    for (int i = 0; i < idai; i++) {
+        if (string[i] == chuoi[k]) {
+            idem[k]++;
+        }
     }
+}
+
 int main() {
-    char string[50];
-    int ia = 0, ie = 0, ii = 0, io = 0, iu = 0, idai;
+
     printf("Enter string:");
     gets(string);
     idai = strlen(string);
-    for (int i = 0; i < idai; i++) {
-        if (string[i] == 'a') {
-            ia++;
-        }
-    }
-    idai = strlen(string);
-    for (int i = 0; i < idai; i++) {
-        if (string[i] == 'e') {
-            ie++;
-        }
-    }
-    idai = strlen(string);
-    for (int i = 0; i < idai; i++) {
-        if (string[i] == 'i') {
-            ii++;
-        }
-    }
-    idai = strlen(string);
-    for (int i = 0; i < idai; i++) {
-        if (string[i] == 'o') {
-            io++;
-        }
-    }
-    idai = strlen(string);
-    for (int i = 0; i < idai; i++) {
-        if (string[i] == 'u') {
-            iu++;
-        }
-    }
 
 
+    idai = strlen(string);
+
+    for (int j = 0; j < 5; ++j) {
+        dem(j);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        iphantram[i] = tinh(idem[i], idai);
+    }
 
     printf("number of characters:\n");
-    printf(" %c: %d; %c: %d; %c: %d; %c: %d; %c: %d;", 'a', ia, 'e', ie, 'i', ii, 'o', io, 'u', iu);
+    for (int i = 0; i < 5; i++) {
+        printf("%c: %d; ", chuoi[i], idem[i]);
+    }
     printf("\nPercentages of total:\n");
-    printf(" %c: %.2f; %c: %.2f; %c: %.2f; %c: %.2f; %c: %.2f ;",'a', tinh(ia,idai), 'e', tinh(ie,idai), 'i',tinh(ii,idai), 'o',tinh(io,idai), 'u', tinh(iu,idai));
+    for (int i = 0; i < 5; i++) {
+        printf("%c: %.2f%c; ", chuoi[i], iphantram[i],'%');
+    }
     return 0;
 }
 
-//
-//
