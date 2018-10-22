e65bb1c1babe658a402470acd7882366672e3d39
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-22 20:07:38 +0700

    ex 2

--- main.c
+++ main.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <mem.h>
 
-char coutput[60], s1[30], s2[30];
+char cword[5] = {'a', 'e', 'i', 'o', 'u'};
+char cword2[5] = {'A', 'E', 'I', 'O', 'U'};
+int idem[5];
+float iphantram[5];
 
-void concat(char s1[], char s2[]) {
-    strcat(coutput, s1);
-    coutput[strlen(coutput)] = ' ';
-    strcat(coutput, s2);
+void nOfcharacters(char s1[]) {
+    for (int i = 0; i < 5; i++) {
+        while (strchr(s1, cword[i]) == NULL || s1, cword2[i] == NULL) {
+            idem[i]++;
+        };
+    }
 }
 
+void pOftotal(char s1[]) {
+    int ia;
+    ia = strlen(s1);
+    for (int i = 0; i < 5; i++) {
+        iphantram[i] = idem[i] / ia * 100;
+    }
+}
+
+
 int main() {
-    printf("Enter the first string: ");
+    char s1[50];
+    printf("Enter string:");
     gets(s1);
-    printf("Enter the second string: ");
-    gets(s2);
-    concat(s1, s2);
-    printf("The concatenated string: %s", coutput);
+    nOfcharacters(s1);
+    pOftotal(s1);
+    printf("number of characters:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%c: %d", cword[i], idem[i]);
+    }
+    printf("Percentages of total:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%c: %d", cword[i], iphantram[i]);
+    }
+
     return 0;
 }
