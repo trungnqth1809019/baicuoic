80ae9229afa9a93b83c8b5cb59f0433fa7f573f1
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-22 19:28:42 +0700

    Exercise 1

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <mem.h>
+
+char coutput[60], s1[30], s2[30];
+
+void concat(char s1[], char s2[]) {
+    strcat(coutput, s1);
+    coutput[strlen(coutput)] = ' ';
+    strcat(coutput, s2);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Enter the first string: ");
+   gets(s1);
+    printf("Enter the second string: ");
+    gets(s2);
+    concat(s1, s2);
+    printf("The concatenated string: %s", coutput);
     return 0;
 }
