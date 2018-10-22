f2dff5397d71ddf6e1ac7c5dc13f6065eef9afae
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-22 18:57:15 +0700

    baitapcuoi

---
+++ cmake-build-debug/CMakeFiles/baitapcuoi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baitapcuoi.dir/main.c.obj"
+  "baitapcuoi.pdb"
+  "baitapcuoi.exe"
+  "baitapcuoi.exe.manifest"
+  "libbaitapcuoi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/baitapcuoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/baitapcuoi.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for baitapcuoi.
+# This may be replaced when dependencies are built.
